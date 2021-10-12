91427e543de98224be09822d24f4586b68fb84e2
FranciscoPugh <franciscopugh01@gmail.com>
2021-10-12 12:29:56 -0300

    Primer clase

---
+++ 1-Conceptos-Generales/script.js
@@ -0,0 +1,48 @@
+/*
+var nombre = "Francisco"
+var apellido = "Pugh"
+
+alert(nombre)
+
+var nombre = "Lautaro"
+
+alert(nombre)
+
+let nombre3
+
+let nombre1 = "Cristian"
+
+alert(nombre1)
+
+nombre1 = "Camila"
+
+console.log(nombre1)
+
+const iva = 1.21
+
+//iva = 1.22
+
+document.write(iva)
+
+
+var1 = 1;
+
+var1 = "Hola Coder!"
+
+//var1 = true
+
+console.log(1 + "1")
+*/
+
+//let nombre = prompt("Ingrese un nombre")
+//console.log(nombre)
+
+
+let numero1 = Number(prompt("Ingrese un numero"))
+
+let numero2 = parseFloat(prompt("Ingrese otro numero"))
+
+let numero3 = parseInt(prompt("Ingrese otro numero"))
+
+alert(numero1 + numero2 + numero3)
+
